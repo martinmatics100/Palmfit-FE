6958df774036cf25347f474173195e575796a05c
Nwatu Martin Chukwuemeka <128225275+martinmatics100@users.noreply.github.com>
2024-06-25 20:16:44 +0100

    still working on the landing page

---
+++ src/components/LandingPage/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./Navbar";
+import "../../App.css";
+import "./Landing.css";
+import HomeFirstPart from "./HomeFirstPart";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <HomeFirstPart />
+    </div>
+  );
+};
+
+export default Home;
