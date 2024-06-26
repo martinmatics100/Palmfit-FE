2c278b8c80ce328146193ac99747f56002ab9a7d
Nwatu Martin Chukwuemeka <128225275+martinmatics100@users.noreply.github.com>
2024-06-26 23:00:06 +0100

    still working on the landing page

--- src/components/LandingPage/Home.js
+++ src/components/LandingPage/Home.js
@@ -3,12 +3,16 @@ import Navbar from "./Navbar";
 import "../../App.css";
 import "./Landing.css";
 import HomeFirstPart from "./HomeFirstPart";
+import AboutSection from "./AboutSection";
+import WhyUs from "./WhyUs";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <HomeFirstPart />
+      <AboutSection />
+      <WhyUs />
     </div>
   );
 };
