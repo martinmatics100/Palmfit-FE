b51635405e3a79c5cb40aac8afa1637aafb6dce3
Nwatu Martin Chukwuemeka <128225275+martinmatics100@users.noreply.github.com>
2024-07-31 02:21:23 +0100

    still working on my palmfit project

--- src/components/New/NewUser/AddNewUser.js
+++ src/components/New/NewUser/AddNewUser.js
@@ -1,8 +1,5 @@
 import React, { useEffect, useState } from "react";
 import "./AddNewUser.scss";
-// import DriverFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
-import { IconButton } from "@mui/material";
-import PhotoCamera from "@mui/icons-material/PhotoCamera";
 import LoadingPage from "../../LoadingPage/LoadingPage";
 
 const AddNewUser = ({ inputs, title }) => {
@@ -14,57 +11,13 @@ const AddNewUser = ({ inputs, title }) => {
       setLoading(false);
     }, 3000);
   }, []);
-  const [file, setFile] = useState("");
   return (
     <div className="loader-container">
       {loading ? (
         <LoadingPage loading={loading} />
       ) : (
         <div>
-          <div className="newUser">
-            <div className="top">
-              <h1>{title}</h1>
-            </div>
-            <div className="bottom">
-              <div className="left">
-                <img
-                  src={
-                    file
-                      ? URL.createObjectURL(file)
-                      : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
-                  }
-                  alt=""
-                />
-              </div>
-              <div className="right">
-                <form>
-                  <div className="formInput">
-                    <label htmlFor="file">
-                      Image: <PhotoCamera className="icon" />
-                    </label>
-                    <input
-                      type="file"
-                      id="file"
-                      onChange={(e) => setFile(e.target.files[0])}
-                      style={{ display: "none" }}
-                    />
-                  </div>
-
-                  {inputs.map((input) => (
-                    <div className="formInput" key={input.id}>
-                      <label>{input.label}</label>
-                      <input
-                        type={input.type}
-                        placeholder={input.placeholder}
-                      />
-                    </div>
-                  ))}
-
-                  <button>Add User</button>
-                </form>
-              </div>
-            </div>
-          </div>
+          <h1>Hello world</h1>
         </div>
       )}
     </div>
